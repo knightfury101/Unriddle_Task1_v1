64d67d564aa4d3be54bee7462eb5b47625aa944a
Aditya Singh <adityasrajput786@gmail.com>
2024-05-31 12:18:57 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import TextEditor from "@/components/TextEditor";
+
+export default function Home() {
+  return (
+    <div className="flex justify-between w-full min-h-screen py-12">
+      <TextEditor />
+    </div>
+  );
+}
---
+++ components/TextEditor.tsx
@@ -0,0 +1,11 @@
+import { Editor } from "novel";
+
+const TextEditor = () => {
+  return (
+    <div className="flex justify-center w-full min-h-screen py-12">
+      <Editor />
+    </div>
+  );
+};
+
+export default TextEditor;
